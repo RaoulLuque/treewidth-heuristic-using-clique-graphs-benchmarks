3846699c2caf2c25312533d689e3e01449b1170d
Raoul Luqué <raoulsluque@gmail.com>
2024-08-05 16:17:04 +0200

    Added variance benchmark

--- benchmark-suites/src/lib.rs
+++ benchmark-suites/src/lib.rs
@@ -47,7 +47,7 @@ use log::debug;
 use petgraph::graph::NodeIndex;
 use HeuristicVariant::*;
 
-pub const TEST_SUITE: [(fn() -> Vec<HeuristicVariant>, &str); 8] = [
+pub const TEST_SUITE: [(fn() -> Vec<HeuristicVariant>, &str); 9] = [
     // DEBUG
     // (test_test_suite, "z_test_test_suite"),
     // Actual Tests
@@ -80,6 +80,7 @@ pub const TEST_SUITE: [(fn() -> Vec<HeuristicVariant>, &str); 8] = [
         comparison_of_bounded_clique_heuristics,
         "comparison_of_bounded_clique_heuristics",
     ),
+    (variance, "variance"),
 ];
 
 pub fn test_if_fill_while_works() -> Vec<HeuristicVariant> {
@@ -140,7 +141,15 @@ pub fn comparison_of_bounded_clique_heuristics() -> Vec<HeuristicVariant> {
         MSTreINegInIBC(2),
         FilWhINiTSdIBC(-1),
         MSTreINegInIBC(-1),
+        FilWhINiTLdIBC(-3),
+        MSTreINegInIBC(-3),
+        FilWhINiTLdIBC(-5),
+        MSTreINegInIBC(-5),
     ]
+}
+
+pub fn variance() -> Vec<HeuristicVariant> {
+    vec![FilWhINiTLd, MSTreINegIn]
 }
 
 impl std::fmt::Display for HeuristicVariant {
